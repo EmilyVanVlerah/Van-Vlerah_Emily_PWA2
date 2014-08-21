dcfb14faaea886f78f2f48e786d1800cf6cd8d5c
EmilyVanVlerah <evanvlerah@fullsail.edu>
2014-08-21 15:35:28 -0400

    troubleshooting code upload

--- Van Vlerah_Emily_PWA2Project/js/main.js
+++ Van Vlerah_Emily_PWA2Project/js/main.js
@@ -42,5 +42,106 @@
     }).eq(0).addClass('current');
 
 
+/****** Login ******/
+    $('#loginButton').click(function(){
+        var user = $('#user').val();
+        var pass = $('#pass').val();
+
+        console.log("This notifies you if the password is working.");
+
+        $.ajax({
+            url: 'whr/login.php',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                username: user,
+                password: pass
+            },
+
+            success:function(response){
+                console.log("Test User");
+                if(response.error){
+                    alert(response.error);
+                }else{
+                    window.location.assign('admin.html');
+                }
+            }
+        });
+    });
+
+
+/****** Logout ******/
+    $('#logOut').click(function(e){
+        e.preventDefault();
+        $.get('xhr/logout.php', function(){
+            window.location.assign('index.html')
+        })
+    });
+
+
+/****** Register ******/
+$('#register').click(function(){
+    var firstname = $('#first').val(),
+        lastname = $('#last').val(),
+        username = $('#userName').val(),
+        email = $('#email').val(),
+        password = $('#password').val();
+
+    console.log(firstname+ '' +lastname+ '' +username+ '' +email+ '' +password);
+
+    $.ajax({
+        url: 'whr/register.php',
+        type: 'post',
+        dataType: 'json',
+        data: {
+            firstname: firstname,
+            lastname: lastname,
+            username: username,
+            email: email,
+            password: password
+        },
+
+        success:function(response){
+            if(response.error){
+                alert(response.error);
+            }else{
+                window.location.assign('admin.html');
+            }
+        }
+    });
+});
+
+
+/****** Project Page Button ******/
+    $('.projectsbtn').on('click', function(e){
+        e.preventDefault();
+        window.location.assign('projects.html');
+    });
+
+
+/****** Add Button ******/
+    $('.addButton').on('click', function(e){
+        e.preventDefault();
+        window.location.assign('projects.html');
+    });
+
+
+/****** Dashboard Button ******/
+    $('.dashboardbtn').on('click', function(e){
+        e.preventDefault();
+        window.location.assign('admin.html');
+    });
+
+
+/****** Display Username ******/
+    $.getJSON("xhr/check_login.php", function(data){
+        console.log(data);
+        $.each(data, function(key, val){
+            console.log(val.first_name);
+            $(".userid").html("Welcome User: " + val.first_name);
+        })
+    });
+
+
 
 })(jQuery);
